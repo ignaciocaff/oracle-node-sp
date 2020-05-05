2cb8186dd2670cda0d5b12ed755343b7edf205e8
jigcaffaratti@gmail.com <jigcaffaratti@gmail.com>
2020-05-05 09:14:25 -0300

    Parametros dinamicos

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,50 @@
+// tslint:disable-next-line: no-var-requires
 const oracledb = require('oracledb');
 const numRows = 100000;
 
 export class StoreProcedureDb {
   public name: string;
   public parameters: any[];
+  public size: number;
 
   public constructor(name: string, parameters?: any[]) {
     this.name = name;
     this.parameters = parameters;
   }
 
-  public executeSp = async () => {
-    async function run() {
-      let connection;
-
-      try {
-        connection = await oracledb.getConnection();
-        const result = await connection.execute(
-          `BEGIN
-               ${this.name}(:id, :cursor);
-               END;`,
-          {
-            id: this.parameters[0],
-            cursor: { type: oracledb.CURSOR, dir: oracledb.BIND_OUT },
-          },
-        );
-
-        const resultSet = result.outBinds.cursor;
-        console.log(result.outBinds);
-        let rows;
-        rows = await resultSet.getRows(numRows);
-        await resultSet.close();
-        console.log(rows);
-      } catch (err) {
-        console.error(err);
-      } finally {
-        if (connection) {
-          try {
-            await connection.close();
-          } catch (err) {
-            console.error(err);
-          }
+  public async executeSp(): Promise<any> {
+    let connection;
+    try {
+      connection = await oracledb.getConnection();
+      let stringParams: string = '';
+      let valueParams: any[] = [];
+      let size = this.parameters.length;
+      if (this.parameters) {
+        for (let i = 0; i < size; i++) {
+          stringParams += `:${i},`;
+          valueParams[i] = { [i] : this.parameters[i] };
         }
       }
+      valueParams[size] = { [size]: { type: oracledb.CURSOR, dir: oracledb.BIND_OUT } };
+      const result = await connection.execute(
+        `BEGIN
+               ${this.name}(${stringParams}:cursor);
+               END;`,
+        valueParams,
+      );
+
+      const resultSet = result.outBinds.cursor;
+      let rows;
+      rows = await resultSet.getRows(numRows);
+      await resultSet.close();
+      return rows;
+    } catch (err) {
+    } finally {
+      if (connection) {
+        try {
+          await connection.close();
+        } catch (err) {}
+      }
     }
-  };
+  }
 }
